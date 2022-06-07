476251914409e582977ec8861ac37161d731cc64
corentintran <102200377+corentintran@users.noreply.github.com>
2022-06-07 15:08:42 +1000

    users

--- index.js
+++ index.js
@@ -6,54 +6,113 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Volcano API' });
 });
 
-router.get('/api', function(req, res, next) {
-  res.render('index', { title: 'Lots of routes available' });
-});
 
-/* Countries */
+
+/* *********** GET Countries *************/
 router.get('/countries', function(req, res, next) {
-  req.db.from('City').select('name','district')
+  req.db.from('data').select('country')
+  .then(rows => rows.map(row=>row.country))
   .then(
-    rows => {
+    country => {
       res.status(200).json({
-        Error: false,
-        Message: "success",
-        City: rows
+        country
       })
     }
   )
   .catch(err => {
     console.log(err);
     res.status(400).json({
-      Error: true,
-      Message: "Invalid query parameters."
+      error: true,
+      message: "Invalid query parameters. Query parameters are not permitted."
     })
   });
 });
 
-/* Volcanoes */
-router.get('/volcanoes/:Country', function(req, res, next) {
-  const country = req.params.Country;
+/* *********  GET Volcanoes ***********/
+router.get('/volcanoes', function(req, res, next) {
+  const country = req.query.country;
 
-  req.db.from('volcanoes').select('*').where('country','=',country)
-  //req.db.raw(`SELECT * FROM City WHERE CountryCode = '${countryCode}'`)
+  if (!country){
+    res.status(400).json({ error: true, message:"Country is a required query parameter." });
+    return;
+  }
+  req.db.from('data').select('id', 'name', 'country', 'region', 'subregion').where({'country':country})
   .then(
-    rows => {
+    volcanoes => {
       res.status(200).json({
-        Error: false,
-        Message: "success",
-        Volcanoes: rows
+        volcanoes
       })
     }
   )
   .catch(err => {
     console.log(err);
     res.status(400).json({
-      Error: true,
-      Message: "Missing country query parameter or query parameters invalid. Click on 'Schema' below to see the possible error responses."
+      error: true,
+      message: "Invalid query parameters. Only country and populatedWithin are permitted."
     })
   });
 });
+
+
+const authorize = function (req, res, next) {
+  const auth = req.headers.authorization;
+  if (!auth || auth.split(" ").length !== 2) {
+    res.status(401).json({
+      Error: true,
+      Message: "Missing or malformed JWT"
+    });
+    return;
+  }
+  const token = auth.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, secretKey);
+    if (Date.now() > payload.exp) {
+      res.status(401).json({
+        Error: true,
+        Message: "Expired JWT"
+      });
+      return;
+    }
+
+    next();
+  } catch (e) {
+    res.status(401).json({
+      Error: true,
+      Message: "Invalid JWT"
+    });
+    return;
+  }
+};
+
+/* ********* GET Volcanoes {id}***********/
+router.get('/volcano/:id', authorize, function(req, res, next) {
+  const id = req.params.country;
+
+  if (!id){
+    res.status(404).json({ error: true, message:"Volcano with ID: 99999 not found." });
+    return;
+  }
+  req.db.from('data').select('id', 'name', 'country', 'region', 'subregion').where({'country':country})
+  .then(
+    volcanoes => {
+      res.status(200).json({
+        volcanoes
+      })
+    }
+  )
+  .catch(err => {
+    console.log(err);
+    res.status(400).json({
+      error: true,
+      message: "Invalid query parameters. Only country and populatedWithin are permitted."
+    })
+  });
+});
+
+
+
+
+
 
 router.post('/api/update', function(req, res, next) {
   if (!req.body.City || !req.body.CountryCode || !req.body.Pop) {
--- users.js
+++ users.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* ******* POST user register *******/
+router.post('user/register', function(req, res, next) {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).json({ error: true, message: "Request body incomplete, both email and password are required" });
+    return;
+  } else {
+  res.status(201).json({
+    message: "User created"
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(409).json({
+      error: true,
+      message: "User already exists"
+    })
+  });
+}
+});
+/* ********* POST user login *******/
+router.post('user/login', function(req, res, next) {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).json({ error: true, message: "Request body incomplete, both email and password are required" });
+    return;
+  } else {
+  res.status(200).json({
+    token: tkn,
+    token_type: 'Bearer',
+    expires_in: date
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(401).json({
+      error: true,
+      message: "Incorrect email or password"
+    })
+  });
+}
+});
+/* ******** GET user profile *******/
+router.get('/user/:email/profile', function(req, res, next) {
+  res.status(200).json({
+
+  })
+});
+/* ************ GET me *************/
+router.get('/me', function(req, res, next) {
+  res.status(200).json({
+    name: 'Corentin Tran',
+    student_number: 'n11231670'
+  })
 });
 
 module.exports = router;
